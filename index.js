aabe2239708c4f5b08961be63ffcc821261ededa
Roberto Persan <rob_berty@hotmail.com>
2021-01-13 23:39:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function verde() {
+    document.getElementById("semaf").src="verde.png";
+}
+
+function amarelo() {
+    document.getElementById("semaf").src="amarelo.png";
+}
+
+function vermelho() {
+    document.getElementById("semaf").src="vermelho.png";
+}
+
+function apag() {
+    document.getElementById("semaf").src="apagado.png";
+}
