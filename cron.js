d72f0ced636f2180abfe5dc1350857d9de9de062
Roberto Persan <rob_berty@hotmail.com>
2021-01-14 22:13:41 -0300

    Uni o cronometro com o semaforo

--- cron.js
+++ cron.js
@@ -1,6 +1,5 @@
 "use strict"
 
-var hh = 0;
 var mm = 0;
 var ss = 0;
 
@@ -20,11 +19,10 @@ function pause() {
 //
 function stop() {
     clearInterval(cron);
-    hh = 0;
     mm = 0;
     ss = 0;
 
-    document.getElementById('counter').innerText ='00:00:00';
+    document.getElementById('counter').innerText ='00:00';
 }
 
 
@@ -35,12 +33,22 @@ function timer() {
         ss = 0;
         mm++;
 
-        if (mm == 60) {
+        if (mm == 28) {
             mm = 0;
-            hh++;
+            document.getElementById('counter').innerText ='00:00';
         }
     }
 
-    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+    var format =(mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
     document.getElementById('counter').innerText = format;
+
+    if (mm < 10){
+        document.getElementById("semaf").src="verde.png";
+    }
+    if (mm == 10 && mm < 13){
+        document.getElementById("semaf").src="amarelo.png";
+    }
+    if (mm == 13 && mm < 28){
+        document.getElementById("semaf").src="vermelho.png";
+    }
 }
