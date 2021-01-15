3732b205f887db28d897310c8dbeed0ba9dedaa5
Roberto Persan <rob_berty@hotmail.com>
2021-01-14 21:55:14 -0300

    Cronometro feito

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict"
 
 function verde() {
     document.getElementById("semaf").src="verde.png";
@@ -14,42 +13,4 @@ function vermelho() {
 
 function apag() {
     document.getElementById("semaf").src="apagado.png";
-}
-
-let mm = 0;
-let ss = 0;
-
-let tempo = 1000; // Quantos milésimos valem 1 segundo?
-var cron;
-
-//
-function start() {
-    cron = setInterval(() => { timer(); }, tempo);
-}
-
-//
-function pause() {
-    clearInterval(cron);
-}
-
-//
-function stop() {
-    clearInterval(cron)
-    mm = 0;
-    ss = 0;
-
-    document.getElementById('counter').innerText ='00:00';
-}
-
-
-function timer() {
-    ss++;
-
-    if (ss == 60){
-        ss = 0;
-        mm++;
-    }
-
-    var format = mm + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss , 10 ? '0' + ss : ss);
-    document.getElementById('counter').immerText = format;
 }
